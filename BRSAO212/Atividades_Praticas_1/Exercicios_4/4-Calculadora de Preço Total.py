987429e0ffe9ed3b350b7a005917a8fc76943525
VictorRdSeS <2019003412@ifam.edu.br>
2026-01-10 13:58:20 -0400

    Todos os exercicios

---
+++ BRSAO212/Atividades_Praticas_1/Exercicios_4/4-Calculadora de Preço Total.py
@@ -0,0 +1,17 @@
+#4- Calculadora de Preço Total
+# Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+
+# Nome do produto: "Cadeira Infantil"
+# Preço unitário: R$ 12.40
+# Quantidade: 3
+#O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+
+valorUni = 12.40
+qtd = 3
+
+total = valorUni * qtd
+
+print("Nome do produto: Cadeira Infantil")
+print("Preço unitário:",valorUni)
+print("Quantidade total:",qtd)
+print("Valor Total R$",total)
